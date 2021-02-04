79a8340a89ed6ddc4c0e3937ad6cb2b0cde005ac
Dan Parkin <danielparkin1@gmail.com>
2021-02-04 19:51:34 +0000

    feat: add prettier config

--- src/extract-variable.ts
+++ src/extract-variable.ts
@@ -1,10 +1,12 @@
 export interface Order {
-    itemPrice: number;
-    quantity: number;
+  itemPrice: number
+  quantity: number
 }
 
 export function calculateOrderTotal(order: Order) {
-    return order.quantity * order.itemPrice -
-        Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-        Math.min(order.quantity * order.itemPrice * 0.1, 100)
+  return (
+    order.quantity * order.itemPrice -
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+    Math.min(order.quantity * order.itemPrice * 0.1, 100)
+  )
 }
